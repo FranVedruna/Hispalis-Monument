f88b7a54137f856c9525c9c7073188830ece168d
FranVedruna <franciscomanuel.martin@a.vedrunasevillasj.es>
2025-05-18 17:05:07 +0200

    Segunda actualización de aplicación sin mejoras a la interfaz

--- APK/app/build.gradle.kts
+++ APK/app/build.gradle.kts
@@ -29,19 +29,40 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
     implementation ("com.google.android.gms:play-services-location:21.0.1")
-    implementation ("com.squareup.picasso:picasso:2.8")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.okhttp3:okhttp:4.9.3")
     implementation ("com.jakewharton.picasso:picasso2-okhttp3-downloader:1.1.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.1")
+    implementation ("com.google.guava:guava:31.1-android")
+    implementation ("com.github.bumptech.glide:glide:4.15.1")
+
+
+    // Para el mapa (OpenStreetMap)
+    implementation ("org.osmdroid:osmdroid-android:6.1.16")
+
+// Para calcular rutas (OSRM)
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.google.android.gms:play-services-maps:18.2.0")
+    implementation ("com.google.android.gms:play-services-location:21.0.1")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.15.1")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.generativeai)
+    implementation(libs.firebase.crashlytics.buildtools)
+    // Navigation
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
